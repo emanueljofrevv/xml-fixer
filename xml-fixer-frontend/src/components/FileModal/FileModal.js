f3eb396522146a89bc323ddf68107f1e1d0b771f
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-21 16:08:15 -0500

    FileModal component was created

---
+++ xml-fixer-frontend/src/components/FileModal/FileModal.js
@@ -0,0 +1,18 @@
+export const FileModal = (fileDetails, onClose) => {
+  const container = document.createElement("div");
+  container.className = "file-modal";
+
+  const modalContent = document.createElement("div");
+  modalContent.className = "modal-content";
+  modalContent.innerHTML = `
+        <h2>File Details</h2>
+        <pre>${JSON.stringify(fileDetails, null, 2)}</pre>
+        <button class="close-btn">Cerrar</button>
+    `;
+
+  modalContent.querySelector(".close-btn").addEventListener("click", onClose);
+
+  container.appendChild(modalContent);
+
+  return container;
+};

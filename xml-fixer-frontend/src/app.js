c9225f2cae3fb8e960c75a8a86758993eb4dc7d6
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-21 16:11:39 -0500

    Initial process for app.jjs file were aded

---
+++ xml-fixer-frontend/src/app.js
@@ -0,0 +1,9 @@
+import { getFiles } from "./services/api.js";
+
+window.addEventListener("load", async () => {
+  const files = await getFiles();
+
+  window.filesData = files;
+
+  console.log("Files**:", files);
+});

e31240d88ee0fe022fcbdae333e3821ec7248151
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-21 16:07:42 -0500

    FileUpload component was created

---
+++ xml-fixer-frontend/src/components/FileUpload/FileUpload.js
@@ -0,0 +1,24 @@
+import { uploadFile } from "./../../services/api.js";
+
+export function FileUpload(onFileUploaded) {
+  const container = document.createElement("div");
+  container.className = "file-upload";
+
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = ".xml";
+  input.addEventListener("change", async () => {
+    const file = input.files[0];
+
+    if (file) {
+      const result = await uploadFile(file);
+
+      console.log("Archivo subido:", result);
+      onFileUploaded(result);
+    }
+  });
+
+  container.appendChild(input);
+
+  return container;
+}

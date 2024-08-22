e17d67a4bf77f0dc32b10d00d1714469f4cf9fd8
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-22 16:15:09 -0500

    Improvmenting parseMarkdown helper function

--- xml-fixer-frontend/src/helpers/parse-markdown.js
+++ xml-fixer-frontend/src/helpers/parse-markdown.js
@@ -1,24 +1,18 @@
-export const parseMarkdown = (markdown) => {
-  let html = markdown;
+export function parseMarkdown(markdown) {
+  const html = markdown
+    .replace(/^#\s(.+)/gm, "<h1>$1</h1>") // h1
+    .replace(/^##\s(.+)/gm, "<h2>$1</h2>") // h2
+    .replace(/^###\s(.+)/gm, "<h3>$1</h3>") // h3
+    .replace(/^####\s(.+)/gm, "<h4>$1</h4>") // h4
+    .replace(/^>\s(.+)/gm, "<blockquote>$1</blockquote>") // blockquote
+    .replace(/^\-\s(.+)/gm, "<li>$1</li>") // li
+    .replace(/^\d+\.\s(.+)/gm, "<li>$1</li>") // li
+    .replace(/\*\*(.+)\*\*/gm, "<strong>$1</strong>") // strong
+    .replace(/\*(.+)\*/gm, "<em>$1</em>") // em
+    .replace(/`(.+)`/gm, "<code>$1</code>") // code
+    .replace(/---/g, "<hr />");
 
-  // Convert headers
-  html = html.replace(/^### (.*$)/gim, "<h3>$1</h3>");
-  html = html.replace(/^## (.*$)/gim, "<h2>$1</h2>");
-  html = html.replace(/^# (.*$)/gim, "<h1>$1</h1>");
+  const formattedHTML = html.replace(/(<li>.*<\/li>)/gm, "<ul>$1</ul>");
 
-  // Convert bold and italic
-  html = html.replace(/\*\*\*(.*)\*\*\*/gim, "<b><i>$1</i></b>");
-  html = html.replace(/\*\*(.*)\*\*/gim, "<b>$1</b>");
-  html = html.replace(/\*(.*)\*/gim, "<i>$1</i>");
-
-  // Convert unordered lists
-  html = html.replace(/^\* (.*)$/gim, "<ul><li>$1</li></ul>");
-
-  // Convert links
-  html = html.replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2">$1</a>');
-
-  // Convert new lines
-  html = html.replace(/\n/gim, "<br>");
-
-  return html.trim(); // Trim any remaining whitespace
-};
+  return formattedHTML;
+}

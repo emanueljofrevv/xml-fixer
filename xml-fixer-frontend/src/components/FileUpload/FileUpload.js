30afcd911edf931901a0f329de3de343745dd481
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-22 15:50:46 -0500

    Deleta all files action was added

--- xml-fixer-frontend/src/components/FileUpload/FileUpload.js
+++ xml-fixer-frontend/src/components/FileUpload/FileUpload.js
@@ -1,9 +1,10 @@
-import { uploadFile } from "./../../services/api.js";
+import { uploadFile, deleteAllFiles } from "./../../services/api.js";
 
-export function FileUpload(onFileUploaded) {
+export function FileUpload(onFileUploaded, onFilesDeleted) {
   const container = document.createElement("div");
   container.className = "file-upload";
 
+  // Input to upload files
   const input = document.createElement("input");
   input.type = "file";
   input.accept = ".xml";
@@ -16,7 +17,21 @@ export function FileUpload(onFileUploaded) {
     }
   });
 
+  // Button to delete files
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "delete-all-btn";
+  deleteButton.textContent = "Delete All Files";
+  deleteButton.addEventListener("click", async () => {
+    const response = await deleteAllFiles();
+    if (response.ok) {
+      onFilesDeleted();
+    } else {
+      console.error("Failed to delete files");
+    }
+  });
+
   container.appendChild(input);
+  container.appendChild(deleteButton);
 
   return container;
 }

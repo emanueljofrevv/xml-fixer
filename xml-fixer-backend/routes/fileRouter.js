c24132f463def192354b03342a50c024f85a3041
Alfredo Scilabra <alfres85@gmail.com>
2024-08-22 16:21:31 -0300

    add delete routes

--- xml-fixer-backend/routes/fileRouter.js
+++ xml-fixer-backend/routes/fileRouter.js
@@ -8,5 +8,7 @@ const router = express.Router();
 router.post('/upload', fileController.uploadFile);
 router.get('/files', fileController.getAllFiles);
 router.get('/file/:fileName', fileController.getFile);
+router.delete('/file/:fileName', fileController.deleteFile);
+router.delete('/files', fileController.deleteAllFiles);
 
 module.exports = router;

b8083bc641bb95c62f65863ce0d58bec3db182d6
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-22 15:07:37 -0500

    Validation to get files when they not exist previosly

--- xml-fixer-frontend/src/app.js
+++ xml-fixer-frontend/src/app.js
@@ -1,6 +1,8 @@
 import { getFiles } from "./services/api.js";
 
 window.addEventListener("load", async () => {
-  const files = await getFiles();
-  window.filesData = files;
+  if (!window.filesData) {
+    const files = await getFiles();
+    window.filesData = files;
+  }
 });

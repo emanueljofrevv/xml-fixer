386cf41a5849f513fd606d5d39ced5949b48d5f1
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-22 15:07:57 -0500

    Refactor to load router correctly

--- xml-fixer-frontend/src/index.js
+++ xml-fixer-frontend/src/index.js
@@ -1,5 +1,5 @@
 import "./app.js";
 import { router } from "./routes/router.js";
 
-window.addEventListener("load", router);
-window.addEventListener("popstate", router);
+window.addEventListener("load", () => router());
+window.addEventListener("popstate", () => router());

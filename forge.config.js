cbf5594af32414fa10d6d2ba815454ea20db8984
Luis Fernando <dev.lfchamorro@gmail.com>
2024-09-03 08:22:20 -0500

    Config added for deian executables

--- forge.config.js
+++ forge.config.js
@@ -1,36 +1,54 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
 module.exports = {
-    packagerConfig: {
-        asar: true,
-        extraResource: [
-            'xml-fixer-backend', // This folder will be placed inside the resources directory
-            'xml-fixer-frontend', // This folder will be placed inside the resources directory
-            'env.json',                // Add the env.json file located in the root directory
-        ],
+  packagerConfig: {
+    asar: true,
+    extraResource: [
+      "xml-fixer-backend", // This folder will be placed inside the resources directory
+      "xml-fixer-frontend", // This folder will be placed inside the resources directory
+      "env.json", // Add the env.json file located in the root directory
+    ],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "linux"],
     },
-    rebuildConfig: {},
-    makers: [
-        {
-            name: '@electron-forge/maker-zip',
+    {
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        name: "xml-fixer",
+      },
+    },
+    {
+      name: "@electron-forge/maker-deb",
+      config: {
+        options: {
+          maintainer: "Test FerChamorro",
+          homepage: "https://google.com",
+          icon: "./bombillo.png",
+          categories: ["Utility"],
         },
-    ],
-    plugins: [
-        {
-            name: '@electron-forge/plugin-auto-unpack-natives',
-            config: {},
-        },
-        // Fuses are used to enable/disable various Electron functionality
-        // at package time, before code signing the application
-        new FusesPlugin({
-            version: FuseVersion.V1,
-            [FuseV1Options.RunAsNode]: false,
-            [FuseV1Options.EnableCookieEncryption]: true,
-            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-            [FuseV1Options.EnableNodeCliInspectArguments]: false,
-            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-            [FuseV1Options.OnlyLoadAppFromAsar]: true,
-        }),
-    ],
+      },
+    },
+  ],
+  plugins: [
+    {
+      name: "@electron-forge/plugin-auto-unpack-natives",
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: false,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: false,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+      [FuseV1Options.OnlyLoadAppFromAsar]: true,
+    }),
+  ],
 };
